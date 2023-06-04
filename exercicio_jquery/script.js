4e66d644346325be36fd43137d8583933317c609
Kyszty <patrickcasimiroz@gmail.com>
2023-06-04 17:07:23 -0300

    Relogio finalizado

--- exercicio_jquery/script.js
+++ exercicio_jquery/script.js
@@ -1,11 +1,85 @@
 $(document).ready(function() {
     $('<span></span>').addClass('header-span').appendTo('header')
     let header = document.querySelector('header')
-    console.log(header)
+    
 
     function atualizarData() {
-        var dataAtual = new Date();
-        $('.header-span').html(dataAtual.toLocaleTimeString() + `<br>` + dataAtual.toLocaleDateString());
+        let dataAtual = new Date();
+        let diaDaSemana = dataAtual.getDay();
+        let mesDoAno = dataAtual.getMonth();
+        
+        let nomeDia = '#'
+        switch (diaDaSemana) {
+            case 0:
+                nomeDia = 'Domingo';
+                break;
+            case 1:
+                nomeDia = 'Segunda-feira';
+                break;
+            case 2:
+                nomeDia = 'Terça-feira';
+                break;
+            case 3:
+                nomeDia = 'Quarta-feira';
+                break;
+            case 4:
+                nomeDia = 'Quinta-feira';
+                break;
+            case 5:
+                nomeDia = 'Sexta-feira';
+                break;
+            case 6:
+                nomeDia = 'Sábado';
+                break;
+            default: 'Unknown error'
+                break;
+        };
+
+        let nomeMes = '#';
+        switch (mesDoAno) {
+            case 0:
+                nomeMes = 'Janeiro';
+                break;
+            case 1:
+                nomeMes = 'Fevereiro';
+                break;
+            case 2:
+                nomeMes = 'Março';
+                break;
+            case 3:
+                nomeMes = 'Abril';
+                break;
+            case 4:
+                nomeMes = 'Maio';
+                break;
+            case 5:
+                nomeMes = 'Junho';
+                break;
+            case 6:
+                nomeMes = 'Julho';
+                break;
+            case 7:
+                nomeMes = 'Agosto';
+                break;
+            case 8:
+                nomeMes = 'Setembro';
+                break;
+            case 9:
+                nomeMes = 'Outubro';
+                break;
+            case 10:
+                nomeMes = 'Novembro';
+                break;
+            case 11:
+                nomeMes = 'Dezembro';
+                break;
+            default: 'Unknown error'
+                break;
+        };
+        
+        
+        $('.header-span').html(dataAtual.toLocaleTimeString() + `<br>${nomeDia}, ${dataAtual.getDate()} de ${nomeMes}`);
+        console.log(nomeDia);
 
     }
 
