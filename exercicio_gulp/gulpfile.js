d082965daf51b20343a68fc5f9287cfdb09cc390
Patrick Casimiro <patrickcasimiroz@gmail.com>
2023-08-10 21:53:54 -0300

    Instalando o gulp-uglify

---
+++ exercicio_gulp/gulpfile.js
@@ -0,0 +1,32 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const path = require('path');
+const gulpIf = require('gulp-if');
+const sourcemaps = require('gulp-sourcemaps');
+
+function ignorePath(file) {
+    const ignoreFolders = [
+        path.join(__dirname, 'src', 'scss', 'mixins'),
+        path.join(__dirname, 'src', 'scss', 'variaveis')
+    ];
+
+    const filePath = file.path;
+
+    return !ignoreFolders.some(folder => filePath.startsWith(folder));
+}
+
+function compileSass() {
+    return gulp.src('./src/scss/**/*.scss', { base: './src/scss' })
+        .pipe(sourcemaps.init())
+        .pipe(gulpIf(ignorePath, sass({
+            outputStyle: 'compressed'
+        })))
+        .pipe(sourcemaps.write('./maps'))
+        .pipe(gulpIf(ignorePath, gulp.dest('./build/styles')));
+}
+
+
+
+exports.default = () => {
+    gulp.watch('./src/scss/**/*.scss', { ignored: false}, gulp.series(compileSass));
+} 
