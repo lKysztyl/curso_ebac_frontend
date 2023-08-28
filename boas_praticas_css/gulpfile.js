c054426293280d9080c0fdd83553f338ae7d5e72
Patrick Casimiro <patrickcasimiroz@gmail.com>
2023-08-28 20:51:41 -0300

    Refactor: instalando o gulp-sourcemaps

--- boas_praticas_css/gulpfile.js
+++ boas_praticas_css/gulpfile.js
@@ -1,9 +1,12 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const sourcemaps = require('gulp-sourcemaps');
 
 function compileSass() {
     return gulp.src('./src/styles/**/*.scss')
-        .pipe(sass())
+        .pipe(sourcemaps.init())
+        .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./build/styles'));
 }
 
